52b9b9de1154db3cc5a59148691245b0428cff61
Pietro <pingi.pie95@gmail.com>
2020-11-23 14:12:21 +0100

    Hand Presence

---
+++ Assets/XR/Script/HandPresence.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public InputDeviceCharacteristics controllerCharacteristics;
+    private InputDevice targetDevice;
+    public GameObject handModelPrefab;
+
+    private GameObject spawnedHandModel;
+    private Animator handAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TryInitialize();
+    }
+
+    void TryInitialize()
+    {
+
+
+        //serve per vedere le caratteristiche dei devices attaccati
+
+        List<InputDevice> devices = new List<InputDevice>();
+
+        //in questo modo specifichiamo solo un determinato device da controllare
+
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        foreach (var item in devices)
+            Debug.Log(item.name + item.characteristics);
+
+        //per sicurezza se ci sono più rightController
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+            handAnimator = spawnedHandModel.GetComponent<Animator>();
+        }
+
+    }
+
+    void UpdateHandAnimation()
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!targetDevice.isValid)
+        {
+            TryInitialize();
+        }
+        else
+        {
+            //per mostrare le mani
+            spawnedHandModel.SetActive(true);
+            UpdateHandAnimation();
+        }
+
+        //per capire quale buttone è stato premuto
+        //ogni bottone ha un output diverso, qui faccio 3 esempi
+
+        /*if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
+            Debug.Log("Pressing Primary Button");
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > .1f)
+            Debug.Log("Trigger Pressed: " + triggerValue);
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue) && primary2DAxisValue != Vector2.zero)
+            Debug.Log("PrimaryTouchPad: " + primary2DAxisValue);*/
+
+    }
+}
