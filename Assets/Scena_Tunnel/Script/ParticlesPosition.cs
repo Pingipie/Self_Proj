bdc66969b0458b26bd508f6a6289fc4d72092882
Pietro <pingi.pie95@gmail.com>
2020-12-11 11:14:12 +0100

    OK Tunnel

---
+++ Assets/Scena_Tunnel/Script/ParticlesPosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlesPosition : MonoBehaviour
+{
+    GameObject Camera;
+    Vector3 offset;
+
+    private void Start()
+    {
+        Camera = GameObject.Find("VR Camera");
+        offset = new Vector3(-13.0f, -1.6f, 341f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Camera.transform.position + offset;
+    }
+}
