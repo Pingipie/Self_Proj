befc8a9d2e7cf54ba03d97e9aca3d2bdd79a81a1
Pietro <pingi.pie95@gmail.com>
2020-12-20 20:29:01 +0100

    Movimento Merch

--- Assets/XR/Script/Force.cs
+++ Assets/XR/Script/Force.cs
@@ -10,10 +10,12 @@ public class Force : MonoBehaviour
     Vector3 offset;
 
     public float magsqr;
+    private bool rot;
     
     // Start is called before the first frame update
     void Start()
     {
+        rot = false;
         rb = this.GetComponent<Rigidbody>();
         player = GameObject.Find("Camera Offset");
     }
@@ -24,9 +26,49 @@ public class Force : MonoBehaviour
         offset = player.transform.position - transform.position;
         magsqr = offset.sqrMagnitude;
 
-        if (magsqr > 1f)
-            rb.AddForce(.01f * offset.normalized / magsqr);
-        else
-            rb.AddForce(new Vector3(Random.Range(-.1f, .1f), Random.Range(-.1f, .1f), Random.Range(-.1f, .1f)));
+        //if (magsqr > 1f)
+          //  rb.AddForce(.01f * offset.normalized / magsqr);
+        //else
+        rb.AddForce(new Vector3(Random.Range(-.5f, .5f), Random.Range(-.5f, .5f), Random.Range(-.5f, .5f)));
+        if (rot == false)
+        {
+            rot = true;
+            StartCoroutine(rotation());
+        }
     }
+    
+    IEnumerator rotation()
+    {
+        float n = 0;
+
+        float i;
+        i = Random.Range(-20f, 20f);
+        float j;
+        j = Random.Range(-20f, 20f);
+        float k;
+        k = Random.Range(-20f, 20f);
+        float a = 0, b = 0, c = 0;
+        while (n == 0)
+        {
+            rb.transform.Rotate(new Vector3(a, b, c));
+            if (i > 0)
+                a = .3f;
+            else if (i < 0)
+                a = -.3f;
+            if (j > 0)
+                b = .3f;
+            else if (j < 0)
+                b = -.3f;
+            if (k > 0)
+                c = .3f;
+            else if (k < 0)
+                c = -.3f;
+            yield return new WaitForSeconds(.01f);
+        }
+    }
+
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        rb.AddForce(collision.contacts[0].normal * 1f);
+    }*/
 }
