daa88d2e8280edaf924aa2d24bb5341706ce397f
Pietro <pingi.pie95@gmail.com>
2020-12-22 19:48:15 +0100

    Inizio RayCast

---
+++ Assets/XR/Script/RaycastTelephone.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastTelephone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.yellow, 1000);
+
+            print(hit.collider.name);
+
+            if (hit.collider.name == "Avatar_Cube")
+            {
+                print("ok hit");
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white, 1000);
+        }
+    }
+}
